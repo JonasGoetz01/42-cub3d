4e6140b8aec650ebaacccddf04872be3a8df7ab9
Jonas Götz <jonas.goetz01@web.de>
2024-06-04 19:02:27 +0200

    logic for displaying a map

--- src/map/showMap.c
+++ src/map/showMap.c
@@ -1,6 +1,65 @@
 #include "../../inc/cub3d.h"
 
+bool isWallAbove(t_global *global, int x, int y)
+{
+    if (y == 0)
+        return (false);
+    if (global->map->map[y - 1][x] == '1')
+        return (true);
+    return (false);
+}
+
+bool isWallBelow(t_global *global, int x, int y)
+{
+    if (y == global->map->height - 1)
+        return (false);
+    if (global->map->map[y + 1][x] == '1')
+        return (true);
+    return (false);
+}
+
+bool isWallLeft(t_global *global, int x, int y)
+{
+    if (x == 0)
+        return (false);
+    if (global->map->map[y][x - 1] == '1')
+        return (true);
+    return (false);
+}
+
+bool isWallRight(t_global *global, int x, int y)
+{
+    if (x == global->map->width - 1)
+        return (false);
+    if (global->map->map[y][x + 1] == '1')
+        return (true);
+    return (false);
+}
+
 void showMap(t_global *global)
 {
-    
+    int xfactor = abs(WIDTH / global->map->width);
+    int yfactor = abs(HEIGHT / global->map->height);
+    if (xfactor > yfactor)
+        xfactor = yfactor;
+    else
+        yfactor = xfactor;
+
+    for (int y = 0; y < global->map->height; y++)
+    {
+        for (int x = 0; x < global->map->width; x++)
+        {
+            if (global->map->map[y][x] == '1')
+            {
+                if (!isWallAbove(global, x, y))
+                    line((t_point){x * xfactor, y * yfactor}, (t_point){(x + 1) * xfactor, y * yfactor}, get_rgba(255, 255, 255, 255), global);
+                if (!isWallBelow(global, x, y))
+                    line((t_point){x * xfactor, (y + 1) * yfactor}, (t_point){(x + 1) * xfactor, (y + 1) * yfactor}, get_rgba(255, 255, 255, 255), global);
+                if (!isWallLeft(global, x, y))
+                    line((t_point){x * xfactor, y * yfactor}, (t_point){x * xfactor, (y + 1) * yfactor}, get_rgba(255, 255, 255, 255), global);
+                if (!isWallRight(global, x, y))
+                    line((t_point){(x + 1) * xfactor, y * yfactor}, (t_point){(x + 1) * xfactor, (y + 1) * yfactor}, get_rgba(255, 255, 255, 255), global);
+            }
+        }
+    }
 }
