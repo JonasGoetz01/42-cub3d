72d0c98b76a9acffca107be9f9fc51dfd0501b13
Jonas Götz <jonas.goetz01@web.de>
2024-06-04 19:02:42 +0200

    remove old line and rect stuff and display map instead

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -8,22 +8,6 @@ void loop(void *param)
     global = (t_global *)param;
     mlx_delete_image(global->mlx, global->img);
     global->img = mlx_new_image(global->mlx, WIDTH, HEIGHT);
-    t_point p1;
-    t_point p2;
-    t_point p3;
-    t_point p4;
-
-    p1.x = 10;
-    p1.y = 10;
-    p2.x = 500;
-    p2.y = 500;
-    p3.x = 500;
-    p3.y = 10;
-    p4.x = 10;
-    p4.y = 500;
-
-    // line(p1, p2, get_rgba(255, 255, 255, 255), global);
-    rect_2(p1, p2, get_rgba(255, 255, 255, 255), global);
-    // rect_4(p1, p2, p3, p4, get_rgba(255, 255, 255, 255), global);
+    showMap(global);
     mlx_image_to_window(global->mlx, global->img, 0, 0);
 }
