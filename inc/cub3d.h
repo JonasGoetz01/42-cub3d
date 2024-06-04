8e24daa74b456eb0977dd03b2f563d43fe8ddaa7
Jonas Götz <jonas.goetz01@web.de>
2024-06-04 12:51:16 +0200

    add header

---
+++ inc/cub3d.h
@@ -0,0 +1,14 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <fcntl.h>
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+
+#define WIDTH 1080
+#define HEIGHT 720
+
+#endif
