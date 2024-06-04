42fc5da2e2d9a410ca0c82f9216eaa2f31d76a88
Jonas Götz <jonas.goetz01@web.de>
2024-06-04 13:39:22 +0200

    add global struct to main

--- src/main.c
+++ src/main.c
@@ -2,18 +2,18 @@
 
 int	main(int argc, char **argv)
 {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
+	t_global global;
 
 	(void)argc;
     (void)argv;
-	mlx = mlx_init(WIDTH, HEIGHT, "cub3d", true);
-	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	global.mlx = mlx_init(WIDTH, HEIGHT, "cub3d", true);
+	global.img = mlx_new_image(global.mlx, WIDTH, HEIGHT);
 	// mlx_key_hook(mlx, quit, &mbt);
 	// mlx_scroll_hook(mlx, scroll, &mbt);
 	// mlx_resize_hook(mlx, resize, &mbt);
 	// mlx_cursor_hook(mlx, mouse_event, &mbt);
 	// mlx_mouse_hook(mlx, mouse_click_event, &mbt);
-	mlx_loop(mlx);
+	mlx_loop_hook(global.mlx, loop, &global);
+	mlx_loop(global.mlx);
 	return (0);
 }
