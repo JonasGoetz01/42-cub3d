ad37a8dfb71c8da0de92af1798246fa5d64c2691
Jonas Götz <jonas.goetz01@web.de>
2024-06-05 22:19:06 +0200

    keys to controll the player

--- src/hooks/key.c
+++ src/hooks/key.c
@@ -9,4 +9,36 @@ void    keyHook(mlx_key_data_t keydata, void* param)
         mlx_close_window(global->mlx);
         return;
     }
+    else if (keydata.key == MLX_KEY_W)
+    {
+        global->player->pos.y -= 1;
+    }
+    else if (keydata.key == MLX_KEY_S)
+    {
+        global->player->pos.y += 1;
+    }
+    else if (keydata.key == MLX_KEY_A)
+    {
+        global->player->pos.x -= 1;
+    }
+    else if (keydata.key == MLX_KEY_D)
+    {
+        global->player->pos.x += 1;
+    }
+    else if (keydata.key == MLX_KEY_LEFT)
+    {
+        global->player->dir.x -= 1;
+    }
+    else if (keydata.key == MLX_KEY_RIGHT)
+    {
+        global->player->dir.x += 1;
+    }
+    else if (keydata.key == MLX_KEY_UP)
+    {
+        global->player->dir.y -= 1;
+    }
+    else if (keydata.key == MLX_KEY_DOWN)
+    {
+        global->player->dir.y += 1;
+    }
 }
