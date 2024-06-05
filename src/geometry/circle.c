b888fbddf815bf547a09937a55562883e1628351
Jonas Götz <jonas.goetz01@web.de>
2024-06-05 21:21:47 +0200

    circle from point

--- src/geometry/circle.c
+++ src/geometry/circle.c
@@ -33,3 +33,37 @@ void    circle(t_circle *circle, t_global *global, uint32_t color)
         }
     }
 }
+
+void    circle_point(int x, int y, int radius, t_global *global, uint32_t color)
+{
+    int x1 = radius - 1;
+    int y1 = 0;
+    int dx = 1;
+    int dy = 1;
+    int err = dx - (radius << 1);
+
+    while (x1 >= y1)
+    {
+        mlx_put_pixel(global->img, x + x1, y + y1, color);
+        mlx_put_pixel(global->img, x + y1, y + x1, color);
+        mlx_put_pixel(global->img, x - y1, y + x1, color);
+        mlx_put_pixel(global->img, x - x1, y + y1, color);
+        mlx_put_pixel(global->img, x - x1, y - y1, color);
+        mlx_put_pixel(global->img, x - y1, y - x1, color);
+        mlx_put_pixel(global->img, x + y1, y - x1, color);
+        mlx_put_pixel(global->img, x + x1, y - y1, color);
+
+        if (err <= 0)
+        {
+            y1++;
+            err += dy;
+            dy += 2;
+        }
+        if (err > 0)
+        {
+            x1--;
+            dx += 2;
+            err += dx - (radius << 1);
+        }
+    }
+}
