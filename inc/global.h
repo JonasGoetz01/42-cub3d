0f08bd18da0b7759f2f5fb402bd9f50738985e7d
Jonas Götz <jonas.goetz01@web.de>
2024-06-05 21:20:22 +0200

    save factors

--- inc/global.h
+++ inc/global.h
@@ -2,12 +2,17 @@
 # define GLOBAL_H
 
 #include "../lib/MLX42/include/MLX42/MLX42.h"
+#include "player.h"
 
 typedef struct s_map t_map;
+
 typedef struct s_global {
     mlx_t		*mlx;
 	mlx_image_t	*img;
     t_map       *map;
+    t_player    *player;
+    int         xfactor;
+    int         yfactor;
 } t_global;
 
 #endif
