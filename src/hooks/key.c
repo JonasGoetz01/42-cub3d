733be1fa4a0f0d553784c780c70c15d4717f5c1e
Jonas Götz <jonas.goetz01@web.de>
2024-06-06 13:44:28 +0200

    better movement

--- src/hooks/key.c
+++ src/hooks/key.c
@@ -4,6 +4,8 @@
 void    keyHook(mlx_key_data_t keydata, void* param)
 {
     t_global *global = (t_global*)param;
+    if(keydata.action == MLX_PRESS)
+        return;
     if (keydata.key == MLX_KEY_ESCAPE)
     {
         mlx_close_window(global->mlx);
@@ -11,34 +13,18 @@ void    keyHook(mlx_key_data_t keydata, void* param)
     }
     else if (keydata.key == MLX_KEY_W)
     {
-        global->player->pos.y -= 1;
+        global->player->pos.y -= 0.1;
     }
     else if (keydata.key == MLX_KEY_S)
     {
-        global->player->pos.y += 1;
+        global->player->pos.y += 0.1;
     }
     else if (keydata.key == MLX_KEY_A)
     {
-        global->player->pos.x -= 1;
+        global->player->pos.x -= 0.1;
     }
     else if (keydata.key == MLX_KEY_D)
     {
-        global->player->pos.x += 1;
-    }
-    else if (keydata.key == MLX_KEY_LEFT)
-    {
-        global->player->dir.x -= 1;
-    }
-    else if (keydata.key == MLX_KEY_RIGHT)
-    {
-        global->player->dir.x += 1;
-    }
-    else if (keydata.key == MLX_KEY_UP)
-    {
-        global->player->dir.y -= 1;
-    }
-    else if (keydata.key == MLX_KEY_DOWN)
-    {
-        global->player->dir.y += 1;
+        global->player->pos.x += 0.1;
     }
 }
