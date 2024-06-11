89a7bf18f1fc3af8ff380770b355352865cd2948
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 23:18:39 +0200

    map the distance of the collision to the height od the rect to draw

--- src/3dview/image.c
+++ src/3dview/image.c
@@ -46,12 +46,41 @@ void    show_sky_and_floor(t_global *global)
     }
 }
 
-void    render_3d(t_global *global)
+float map_distance_to_height(float distance, t_global *global)
 {
+    float height;
+    int window_height;
+
+    window_height = global->window_height;
+
+    if (distance == 0)
+        height = window_height;
+    else
+        height = (global->scale_factor / distance) * window_height;
+
+    if (height > window_height)
+        height = window_height;
+
+    return height;
+}
+
+
+float   get_distance(t_vec2d a, t_vec2d b)
+{
+    return (sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2)));
+}
+
+void render_3d(t_global *global)
+{
+    int bar_width = global->img->width / NUM_RAYS;
     for (int i = 0; i < NUM_RAYS; i++)
     {
-        // t_ray *ray = &global->player->rays[i];
-        int height = global->img->height / 2;
-        draw_bar(global, i * global->img->width / NUM_RAYS, (global->img->height / 2) - (height / 2), global->img->width / NUM_RAYS, global->img->height / 2, get_rgba(255, 255, 255, 255));
+        float distance = get_distance(global->player->pos, *(global->player->rays[i].closest_collision));
+        int bar_height = map_distance_to_height(distance, global);
+        int center_y = global->img->height / 2;
+        int top_y = center_y - (bar_height / 2);
+        int x = i * bar_width;
+
+        draw_bar(global, x, top_y, bar_width, bar_height, get_rgba(255, 255, 255, 255));
     }
 }
