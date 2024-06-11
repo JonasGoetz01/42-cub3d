13a7cf6e636821ae611b22b7583b1c1070a0a736
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 15:15:04 +0200

    move and rotate player

--- src/player/player.c
+++ src/player/player.c
@@ -30,3 +30,29 @@ t_player *new_player(t_vec2d pos, t_vec2d dir) {
     return (player);
 }
 
+// update the players position and all the rays
+void update_position(t_global *global, t_vec2d pos) {
+    global->player->pos = pos;
+    for (int i = 0; i < NUM_RAYS; i++) {
+        global->player->rays[i].origin = pos;
+    }
+}
+
+void rotate_player(t_global *global, float angle) {
+    float cos_angle = cosf(angle);
+    float sin_angle = sinf(angle);
+    t_vec2d old_dir = global->player->dir;
+    t_vec2d new_dir;
+    float new_x = old_dir.x * cos_angle - old_dir.y * sin_angle;
+    float new_y = old_dir.x * sin_angle + old_dir.y * cos_angle;
+    new_dir = (t_vec2d){new_x, new_y};
+    global->player->dir = new_dir;
+
+    float angle_increment = FOV / (NUM_RAYS - 1);
+    float base_angle = atan2f(new_dir.y, new_dir.x) - (FOV / 2.0f);
+
+    for (int i = 0; i < NUM_RAYS; i++) {
+        float ray_angle = base_angle + (i * angle_increment);
+        global->player->rays[i].direction = (t_vec2d){cosf(ray_angle), sinf(ray_angle)};
+    }
+}
