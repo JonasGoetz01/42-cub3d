09c5943c072c06b4015bc226b610ea969d88b443
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 14:25:56 +0200

    get scale factor

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -16,3 +16,10 @@ int get_rgba(int r, int g, int b, int a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
 }
+
+float calculate_scale_factor(int map_width, int map_height, int window_width, int window_height) {
+    float scale_x = (float)window_width / (float)map_width;
+    float scale_y = (float)window_height / (float)map_height;
+
+    return (scale_x < scale_y) ? scale_x : scale_y;
+}
