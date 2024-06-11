4134201895306821af4521d4efb2883f93d0e248
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 19:12:35 +0200

    make the right border visible on the screen

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 13:03:03 by jgotz             #+#    #+#             */
-/*   Updated: 2024/06/11 17:30:45 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/06/11 19:04:53 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int get_rgba(int r, int g, int b, int a)
 }
 
 float calculate_scale_factor(int map_width, int map_height, int window_width, int window_height) {
-    float scale_x = (float)window_width / (float)map_width;
-    float scale_y = (float)window_height / (float)map_height;
+    float scale_x = ((float)window_width - 10) / (float)map_width;
+    float scale_y = ((float)window_height - 10) / (float)map_height;
 
     return (scale_x < scale_y) ? scale_x : scale_y;
 }
