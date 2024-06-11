f7b40b6eb543fba59823d177d42d5faea2eed6a4
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 13:01:01 +0200

    remove bloat and do in a simpler way

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -8,6 +8,6 @@ void loop(void *param)
     global = (t_global *)param;
     mlx_delete_image(global->mlx, global->img);
     global->img = mlx_new_image(global->mlx, WIDTH, HEIGHT);
-    showMap(global);
+    // showMap(global);
     mlx_image_to_window(global->mlx, global->img, 0, 0);
 }
