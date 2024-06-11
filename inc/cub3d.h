9da330550ee99df57f61b40c72fdb04aa0334444
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 12:59:43 +0200

    remove bloat and do in a simpler way

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -8,22 +8,39 @@
 # include <fcntl.h>
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include "../lib/libft/libft.h"
-#include "point.h"
-#include "line.h"
-#include "global.h"
-#include "color.h"
-#include "rect.h"
-#include "map.h"
-#include "hooks.h"
-#include "circle.h"
-#include "ray.h"
-#include "player.h"
+#include "geometry.h"
 
 #define WIDTH 1080
 #define HEIGHT 720
 #define NUM_RAYS 30
 #define FOV 60
+#define BPP sizeof(int32_t)
+
+typedef struct s_map
+{
+    char **map;
+    int width;
+    int height;
+} t_map;
+
+typedef struct s_player
+{
+    t_vec2d pos;
+    t_vec2d dir;
+    t_ray *rays;
+} t_player;
+
+typedef struct s_global
+{
+    mlx_t *mlx;
+    mlx_image_t *img;
+    t_map *map;
+    t_player *player;
+} t_global;
 
 void    loop(void *param);
+void    keyHook(mlx_key_data_t keydata, void* param);
+void    initMap(t_global *global);
+void map_to_line_segments(t_global *global, t_line **lines, int *line_count);
 
 #endif
