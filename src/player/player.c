c64c7e0bf4df5b3a3e0ed0c5db26bbcb1feeed7c
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 14:27:47 +0200

    create the player with new structs

--- src/player/player.c
+++ src/player/player.c
@@ -1,8 +1,10 @@
 #include "../../inc/cub3d.h"
 
-t_player *new_player(t_vec2d pos, t_vec2d dir)
-{
+t_player *new_player(t_vec2d pos, t_vec2d dir) {
     t_player *player;
+    float angle_increment;
+    float ray_angle;
+    int i;
 
     player = malloc(sizeof(t_player));
     if (!player)
@@ -12,16 +14,19 @@ t_player *new_player(t_vec2d pos, t_vec2d dir)
     player->dir = dir;
     
     player->rays = malloc(sizeof(t_ray) * NUM_RAYS);
-    if (!player->rays)
-    {
+    if (!player->rays) {
         free(player);
         return (NULL);
     }
 
-    // for (i = 0; i < NUM_RAYS; i++)
-    // {
-    //     player->rays[i] = new_ray(i * 360 / NUM_RAYS);
-    // }
+    // Calculate angle increment including the edges of the FOV
+    angle_increment = FOV / (NUM_RAYS - 1);
+    for (i = 0; i < NUM_RAYS; i++) {
+        ray_angle = atan2f(player->dir.y, player->dir.x) - (FOV / 2.0f) + (i * angle_increment);
+        player->rays[i].origin = player->pos;
+        player->rays[i].direction = (t_vec2d){cosf(ray_angle), sinf(ray_angle)};
+    }
     
     return (player);
 }
+
