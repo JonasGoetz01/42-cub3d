2982b1ab1d61643a3622c594ed4837a213a20efa
Jonas Götz <jonas.goetz01@web.de>
2024-06-11 12:57:32 +0200

    remove bloat and do in a simpler way

--- inc/color.h
+++
@@ -1,6 +0,0 @@
-#ifndef COLOR_H
-# define COLOR_H
-
-int get_rgba(int r, int g, int b, int a);
-
-#endif
--- inc/hooks.h
+++
@@ -1,8 +0,0 @@
-#ifndef HOOKS_H
-#define HOOKS_H
-
-# include "../lib/MLX42/include/MLX42/MLX42.h"
-
-void    keyHook(mlx_key_data_t keydata, void* param);
-
-#endif
