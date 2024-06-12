257d736ab3847efbea237655f175031519bd3ca9
Jonas Götz <jonas.goetz01@web.de>
2024-06-12 15:18:26 +0200

    different colors for faces

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -14,11 +14,16 @@
 #define HEIGHT 720
 #define FOV 60.0f * (M_PI / 180.0f) // Convert degrees to radians
 #define BPP sizeof(int32_t)
-#define MOVE_SPEED 3.0f
+#define MOVE_SPEED 1.0f
 #define MINIMAP_SCALE 0.3f
 #define SHOW_MINIMAP 0
 #define SHOW_RAYS 0
 #define SHOW_COLLISIONS 0
+
+#define NORTH_COLOR get_rgba(255, 255, 200, 255) // creme
+#define SOUTH_COLOR get_rgba(100, 255, 100, 255) // green
+#define EAST_COLOR get_rgba(100, 10, 10, 255) // brown
+#define WEST_COLOR get_rgba(140, 20, 100, 255) // purple
 
 typedef struct s_map
 {
@@ -36,6 +41,7 @@ typedef struct s_player
 
 typedef struct s_global
 {
+    unsigned long time;
     mlx_t *mlx;
     mlx_image_t *minimap;
     mlx_image_t *img;
@@ -50,7 +56,7 @@ typedef struct s_global
 } t_global;
 
 void    loop(void *param);
-void    keyHook(mlx_key_data_t keydata, void* param);
+void    keyHook(void* param);
 void    initMap(t_global *global);
 void    map_to_line_segments(t_global *global, t_line **lines, int *line_count);
 void    draw_line(t_global *global, t_vec2d a, t_vec2d b);
@@ -61,7 +67,7 @@ void    draw_circle(t_global *global, t_circle *circle, int color);
 int     get_rgba(int r, int g, int b, int a);
 void    draw_ray(t_global *global, t_ray *ray);
 t_player *new_player(t_global *global, t_vec2d pos, t_vec2d dir);
-t_vec2d ray_line_collision(t_ray *ray, t_line *line);
+t_vec2d ray_line_collision(t_ray *ray, t_line *line, t_face *face);
 void    update_position(t_global *global, t_vec2d pos);
 void    rotate_player(t_global *global, float angle);
 void    raycast(t_global *global);
@@ -72,5 +78,6 @@ void    show_sky_and_floor(t_global *global);
 void    make_background_transparent(t_global *global);
 void    render_3d(t_global *global);
 void    draw_bar(t_global *global, int x, int y, int width, int height, int color);
+unsigned long get_current_millis(void);
 
 #endif
