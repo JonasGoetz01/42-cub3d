7df1163ab2435356e68995f842f40154452fdaae
Jonas Götz <jonas.goetz01@web.de>
2024-06-12 15:19:40 +0200

    make key hook a loop hoock for smoothness and multiple keys a time

--- src/hooks/key.c
+++ src/hooks/key.c
@@ -1,40 +1,38 @@
 #include "../../inc/cub3d.h"
 
-void    keyHook(mlx_key_data_t keydata, void* param)
+void    keyHook(void* param)
 {
     t_global *global = (t_global*)param;
-    if(keydata.action == MLX_PRESS)
-        return;
-    if (keydata.key == MLX_KEY_ESCAPE)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_ESCAPE))
     {
         mlx_close_window(global->mlx);
         return;
     }
-    if (keydata.key == MLX_KEY_W)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_W))
     {
         t_vec2d new_pos = {global->player->pos.x + global->player->dir.x * MOVE_SPEED * global->minimap_scale, global->player->pos.y + global->player->dir.y * MOVE_SPEED * global->minimap_scale};
         update_position(global, new_pos);
     }
-    if (keydata.key == MLX_KEY_S)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_S))
     {
         t_vec2d new_pos = {global->player->pos.x - global->player->dir.x * MOVE_SPEED * global->minimap_scale, global->player->pos.y - global->player->dir.y * MOVE_SPEED * global->minimap_scale};
         update_position(global, new_pos);
     }
-    if (keydata.key == MLX_KEY_D)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_D))
     {
         t_vec2d new_pos = {global->player->pos.x - global->player->dir.y * MOVE_SPEED * global->minimap_scale, global->player->pos.y + global->player->dir.x * MOVE_SPEED * global->minimap_scale};
         update_position(global, new_pos);
     }
-    if (keydata.key == MLX_KEY_A)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_A))
     {
         t_vec2d new_pos = {global->player->pos.x + global->player->dir.y * MOVE_SPEED * global->minimap_scale, global->player->pos.y - global->player->dir.x * MOVE_SPEED * global->minimap_scale};
         update_position(global, new_pos);
     }
-    if (keydata.key == MLX_KEY_E)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_E))
     {
         rotate_player(global, 0.1f);
     }
-    if (keydata.key == MLX_KEY_Q)
+    if (mlx_is_key_down(global->mlx, MLX_KEY_Q))
     {
         rotate_player(global, -0.1f);
     }
