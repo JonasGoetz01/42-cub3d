23010f15027142d24b00fe5f21750d11b254da62
Jonas Götz <jonas.goetz01@web.de>
2024-06-13 10:33:43 +0200

    add a custom logo to the program (does not work on macos)

--- src/main.c
+++ src/main.c
@@ -11,11 +11,19 @@ int	main(int argc, char **argv)
 	global.window_width = WIDTH;
 	global.minimap_scale = MINIMAP_SCALE;
 	global.time = get_current_millis();
+	mlx_texture_t *logo = mlx_load_png("textures/wolfenstein-logo.png");
+	if (!logo)
+	{
+		printf("Failed to load logo\n");
+		printf("%s\n", mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
 	if (!(global.mlx = mlx_init(global.window_width, global.window_height, "cub3d", true)))
 	{
 		printf("%s\n", mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
+	mlx_set_icon(global.mlx, logo);
 	if (!(global.minimap = mlx_new_image(global.mlx, global.window_width, global.window_height)))
 	{
 		mlx_close_window(global.mlx);
