e67e5b408fa3d652e8a946c38d81b448af3b7838
Jonas Götz <jonas.goetz01@web.de>
2024-07-11 12:19:02 +0200

    display and load sprite

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -2,12 +2,24 @@
 
 void	loop(void *param)
 {
-	t_global	*global;
-	static int	fps_timer = 0;
-	double		current_time;
-	double		elapsed_time;
-	int			fps;
+	t_global				*global;
+	static int				fps_timer = 0;
+	double					current_time;
+	double					elapsed_time;
+	int						fps;
+	static mlx_texture_t	*sprite1 = NULL;
+	static mlx_texture_t	*sprite2 = NULL;
+	static mlx_texture_t	*sprite3 = NULL;
+	static mlx_texture_t	*sprite4 = NULL;
 
+	if (!sprite1)
+		sprite1 = mlx_load_png("textures/gun1.png");
+	if (!sprite2)
+		sprite2 = mlx_load_png("textures/gun2.png");
+	if (!sprite3)
+		sprite3 = mlx_load_png("textures/gun3.png");
+	if (!sprite4)
+		sprite4 = mlx_load_png("textures/gun4.png");
 	global = (t_global *)param;
 	current_time = get_current_millis();
 	elapsed_time = current_time - global->time;
@@ -25,6 +37,7 @@ void	loop(void *param)
 	global->minimap = mlx_new_image(global->mlx, global->window_width
 			* global->minimap_scale, global->window_height
 			* global->minimap_scale);
+	global->sprite = mlx_texture_to_image(global->mlx, sprite1);
 	global->img = mlx_new_image(global->mlx, global->window_width,
 			global->window_height);
 	make_background_transparent(global);
@@ -32,6 +45,9 @@ void	loop(void *param)
 	show_sky_and_floor(global);
 	render_3d(global);
 	mlx_image_to_window(global->mlx, global->minimap, 0, 0);
+	mlx_image_to_window(global->mlx, global->sprite, global->window_width
+		- global->sprite->width, global->window_height
+		- global->sprite->height);
 	mlx_image_to_window(global->mlx, global->img, 0, 0);
 	if (SHOW_MINIMAP)
 	{
@@ -39,6 +55,9 @@ void	loop(void *param)
 		if (global->minimap->count > 0)
 			mlx_set_instance_depth(global->minimap->instances, 1);
 	}
+	mlx_image_to_window(global->mlx, global->sprite, global->window_width
+		- global->sprite->width, global->window_height
+		- global->sprite->height);
 	mlx_image_to_window(global->mlx, global->img, 0, 0);
 	if (global->img->count > 0)
 		mlx_set_instance_depth(global->img->instances, 0);
