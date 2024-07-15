543c3ccb5cc1535e8cfcf63f0c0fed9d53c48ee2
Jonas Götz <jonas.goetz01@web.de>
2024-07-15 09:37:04 +0200

    no additional sprite image -> put onto other image

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -1,31 +1,58 @@
 #include "../../inc/cub3d.h"
 
+void	ft_texture_to_image(t_global *global, mlx_texture_t *texture)
+{
+	int		i;
+	int		i_start;
+	int		j;
+	int		j_start;
+	int		color;
+	uint8_t	*pixel;
+
+	i = global->window_height - texture->height;
+	i_start = i;
+	while (i < global->window_height)
+	{
+		j = global->window_width - texture->width;
+		j_start = j;
+		while (j < global->window_width)
+		{
+			pixel = &(texture->pixels[((i - i_start) * texture->width + (j
+							- j_start)) * texture->bytes_per_pixel]);
+			if (pixel != NULL)
+			{
+				color = get_rgba(pixel[0], pixel[1], pixel[2], 255);
+				if (pixel[3] != 0)
+				{
+					if ((uint32_t)i < global->img->height
+						&& (uint32_t)j < global->img->width)
+					{
+						mlx_put_pixel(global->img, j, i, color);
+					}
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 void	loop(void *param)
 {
-	t_global				*global;
-	static int				fps_timer = 0;
-	double					current_time;
-	double					elapsed_time;
-	int						fps;
-	static mlx_texture_t	*sprite1 = NULL;
-	static mlx_texture_t	*sprite2 = NULL;
-	static mlx_texture_t	*sprite3 = NULL;
-	static mlx_texture_t	*sprite4 = NULL;
+	t_global	*global;
+	static int	fps_timer = 0;
+	double		current_time;
+	double		elapsed_time;
+	int			fps;
 
-	if (!sprite1)
-		sprite1 = mlx_load_png("textures/gun1.png");
-	if (!sprite2)
-		sprite2 = mlx_load_png("textures/gun2.png");
-	if (!sprite3)
-		sprite3 = mlx_load_png("textures/gun3.png");
-	if (!sprite4)
-		sprite4 = mlx_load_png("textures/gun4.png");
 	global = (t_global *)param;
 	current_time = get_current_millis();
 	elapsed_time = current_time - global->time;
 	global->time = current_time;
+	// Change the sprite every 10 frames
 	if (fps_timer % 10 == 0)
 	{
+		global->sprite_index = (global->sprite_index + 1) % 4;
 		fps = 1000 / elapsed_time;
 		printf("\rFPS: %d", fps);
 		fflush(stdout);
@@ -37,17 +64,14 @@ void	loop(void *param)
 	global->minimap = mlx_new_image(global->mlx, global->window_width
 			* global->minimap_scale, global->window_height
 			* global->minimap_scale);
-	global->sprite = mlx_texture_to_image(global->mlx, sprite1);
 	global->img = mlx_new_image(global->mlx, global->window_width,
 			global->window_height);
 	make_background_transparent(global);
 	showMap(global);
 	show_sky_and_floor(global);
 	render_3d(global);
+	ft_texture_to_image(global, global->sprite_textures[global->sprite_index]);
 	mlx_image_to_window(global->mlx, global->minimap, 0, 0);
-	mlx_image_to_window(global->mlx, global->sprite, global->window_width
-		- global->sprite->width, global->window_height
-		- global->sprite->height);
 	mlx_image_to_window(global->mlx, global->img, 0, 0);
 	if (SHOW_MINIMAP)
 	{
@@ -55,9 +79,6 @@ void	loop(void *param)
 		if (global->minimap->count > 0)
 			mlx_set_instance_depth(global->minimap->instances, 1);
 	}
-	mlx_image_to_window(global->mlx, global->sprite, global->window_width
-		- global->sprite->width, global->window_height
-		- global->sprite->height);
 	mlx_image_to_window(global->mlx, global->img, 0, 0);
 	if (global->img->count > 0)
 		mlx_set_instance_depth(global->img->instances, 0);
