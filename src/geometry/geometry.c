23a9786ad303ab2a2102aefd5cb219be5d42221f
Jonas Götz <jonas.goetz01@web.de>
2024-07-16 10:01:53 +0200

    norm

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -147,129 +147,28 @@ void	draw_ray(t_global *global, t_ray *ray)
 	draw_line(global, ray->origin, end, get_rgba(255, 255, 255, 255));
 }
 
-t_vec2d	ray_line_collision(t_ray *ray, t_line *line, t_face *face)
-{
-	float	x1;
-	float	y1;
-	float	x2;
-	float	y2;
-	float	x3;
-	float	y3;
-	float	x4;
-	float	y4;
-	float	denominator;
-	float	t;
-	float	u;
-	t_vec2d	collision_point;
-
-	x1 = ray->origin.x;
-	y1 = ray->origin.y;
-	x2 = ray->origin.x + ray->direction.x;
-	y2 = ray->origin.y + ray->direction.y;
-	x3 = line->a.x;
-	y3 = line->a.y;
-	x4 = line->b.x;
-	y4 = line->b.y;
-	denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	if (denominator == 0)
-		return ((t_vec2d){-1, -1});
-	t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denominator;
-	u = ((x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)) / denominator;
-	if (t >= 0 && u >= 0 && u <= 1)
-	{
-		collision_point = (t_vec2d){x1 + t * (x2 - x1), y1 + t * (y2 - y1)};
-		if (line->alignment == HORIZONTAL)
-		{
-			if (ray->origin.y < collision_point.y)
-				*face = SOUTH;
-			else
-				*face = NORTH;
-		}
-		else // VERTICAL
-		{
-			if (ray->origin.x < collision_point.x)
-				*face = EAST;
-			else
-				*face = WEST;
-		}
-		return (collision_point);
-	}
-	return ((t_vec2d){-1, -1});
-}
-
-t_collision	*new_collision(t_collision *collisions, int *collision_count,
-		t_vec2d point, t_line *line, t_face face)
-{
-	t_collision	*new_collisions;
-
-	new_collisions = realloc(collisions, (*collision_count + 1)
-			* sizeof(t_collision));
-	if (!new_collisions)
-		return (NULL);
-	new_collisions[*collision_count].point = point;
-	new_collisions[*collision_count].line = line;
-	new_collisions[*collision_count].face = face;
-	(*collision_count)++;
-	return (new_collisions);
-}
-
-void	raycast(t_global *global)
-{
-	t_vec2d		intersection;
-	t_face		face;
-	t_collision	*new_collisions;
-	float		min_distance;
-	float		distance;
-
-	for (int i = 0; i < (int)global->img->width; i++)
-	{
-		for (int j = 0; j < global->line_count; j++)
-		{
-			intersection = ray_line_collision(&global->player->rays[i],
-					&global->lines[j], &face);
-			if (intersection.x != -1)
-			{
-				new_collisions = new_collision(global->player->rays[i].collisions,
-						&global->player->rays[i].collision_count, intersection,
-						&global->lines[j], face);
-				if (!new_collisions)
-					return ;
-				global->player->rays[i].collisions = new_collisions;
-			}
-		}
-		min_distance = 1000000;
-		for (int j = 0; j < global->player->rays[i].collision_count; j++)
-		{
-			distance = sqrtf(powf(global->player->pos.x
-						- global->player->rays[i].collisions[j].point.x, 2)
-					+ powf(global->player->pos.y
-						- global->player->rays[i].collisions[j].point.y, 2));
-			if (distance < min_distance)
-			{
-				min_distance = distance;
-				global->player->rays[i].closest_collision = &global->player->rays[i].collisions[j];
-			}
-		}
-	}
-}
-
-// draw a bar from the center of the screen "width" pixels wide and "height" pixels tall
 void	draw_bar(t_global *global, int x, int y, int width, int height,
 		int color)
 {
 	int	draw_y;
+	int	i;
+	int	j;
 
-	for (int i = 0; i < width; i++)
+	i = 0;
+	while (i < width)
 	{
 		if (x + i < 0 || (uint32_t)x + (uint32_t)i >= global->img->width)
 			continue ;
-		for (int j = 0; j < height; j++)
+		j = 0;
+		while (j < height)
 		{
 			draw_y = y + j;
 			if (draw_y >= 0 && (uint32_t)draw_y < global->img->height)
 			{
 				mlx_put_pixel(global->img, x + i, draw_y, color);
 			}
+			j++;
 		}
+		i++;
 	}
 }
