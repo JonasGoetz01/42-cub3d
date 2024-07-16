1d09932a7430ef1a9d17b0b757956762e8dad2df
Jonas Götz <jonas.goetz01@web.de>
2024-07-16 10:03:03 +0200

    norm

--- src/geometry/raycast1.c
+++ src/geometry/raycast1.c
@@ -19,3 +19,30 @@ static void	add_collision(t_ray *ray, t_vec2d intersection, t_line *line,
 	if (new_collisions)
 		ray->collisions = new_collisions;
 }
+
+static void	find_closest_collision(t_global *global, t_ray *ray)
+{
+	float		distance;
+	float		min_distance;
+	t_vec2d		player_pos;
+	t_collision	*collisions;
+	int			count;
+
+	min_distance = 1000000;
+	player_pos = global->player->pos;
+	collisions = ray->collisions;
+	count = ray->collision_count;
+	global->i = 0;
+	while (global->i < count)
+	{
+		distance = sqrtf(powf(player_pos.x - collisions[global->i].point.x, 2)
+				+ powf(player_pos.y - collisions[global->i].point.y, 2));
+		if (distance < min_distance)
+		{
+			min_distance = distance;
+			ray->closest_collision = &collisions[global->i];
+		}
+		global->i++;
+	}
+}
+
