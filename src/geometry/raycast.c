70d4d093bbe8b06e33be582928702b136e2ae50f
Jonas Götz <jonas.goetz01@web.de>
2024-07-16 10:02:23 +0200

    norm

--- src/geometry/raycast.c
+++ src/geometry/raycast.c
@@ -6,3 +6,15 @@ void	get_denominator(float *denominator, t_ray *ray, t_line *line)
 		* (line->a.y - line->b.y) - (ray->origin.y - (ray->origin.y
 				+ ray->direction.y)) * (line->a.x - line->b.x);
 }
+
+void	get_t_and_u(float *t, float *u, t_ray *ray, t_line *line,
+		float *denominator)
+{
+	*t = ((ray->origin.x - line->a.x) * (line->a.y - line->b.y) - (ray->origin.y
+				- line->a.y) * (line->a.x - line->b.x)) / *denominator;
+	*u = ((ray->origin.x - line->a.x) * (ray->origin.y - (ray->origin.y
+					+ ray->direction.y)) - (ray->origin.y - line->a.y)
+			* (ray->origin.x - (ray->origin.x + ray->direction.x)))
+		/ *denominator;
+}
+
