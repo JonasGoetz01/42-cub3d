9b0e3b6b92bf7a18d94e69565ff5e3fd66fc428c
Jonas Götz <jonas.goetz@egotec.com>
2024-07-17 18:04:24 +0200

    better typecast

--- src/geometry/raycast.c
+++ src/geometry/raycast.c
@@ -8,7 +8,7 @@ void	get_denominator(float *denominator, t_ray *ray, t_line *line)
 }
 
 void	get_t_and_u(float *t, float *u, t_ray *ray, t_line *line,
-		float *denominator)
+		const float *denominator)
 {
 	*t = ((ray->origin.x - line->a.x) * (line->a.y - line->b.y) - (ray->origin.y
 				- line->a.y) * (line->a.x - line->b.x)) / *denominator;
