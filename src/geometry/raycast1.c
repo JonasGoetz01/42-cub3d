dce47ed1d4109c9f3366aa6cf9249eca580e9ef8
Jonas Götz <jonas.goetz@egotec.com>
2024-07-17 18:04:26 +0200

    better typecast

--- src/geometry/raycast1.c
+++ src/geometry/raycast1.c
@@ -1,11 +1,8 @@
 #include "../../inc/cub3d.h"
 
 void		get_denominator(float *denominator, t_ray *ray, t_line *line);
-void		get_t_and_u(float *t, float *u, t_ray *ray, t_line *line,
-				float *denominator);
 void		set_faces(t_ray *ray, t_vec2d collision_point, t_face *face,
 				t_line *line);
-t_vec2d		ray_line_collision(t_ray *ray, t_line *line, t_face *face);
 t_collision	*new_collision(t_collision *collisions, int *collision_count,
 				t_vec2d point, t_line *line, t_face face);
 
