e05c17987019ceacd2dc1736f60878e2e2b70f63
Jonas Götz <jonas.goetz@egotec.com>
2024-07-20 00:53:55 +0200

    norm

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -49,14 +49,68 @@ void my_usleep(unsigned int microseconds) {
 }
 
 
-void	loop(void *param)
+void    cap_fps(double *elapsed_time, double frame_duration, t_global *global)
 {
-	t_global	*global;
-	static int	fps_timer = 0;
-	double		current_time;
-	double		elapsed_time;
-	int			fps;
-	t_line		crosshair[2];
+    double current_time;
+
+    if (*elapsed_time < frame_duration)
+    {
+        my_usleep((frame_duration - *elapsed_time) * 1000); // usleep takes microseconds
+        current_time = get_current_millis();
+        *elapsed_time = current_time - global->time;
+        global->time = current_time;
+    }
+}
+
+void    display_fps(int *fps_timer, double elapsed_time)
+{
+    int fps;
+
+    if (*fps_timer % 10 == 0)
+    {
+        fps = (int)(1000.0 / elapsed_time);
+        write(1, "FPS: ", 5);
+        ft_putnbr_fd(fps, 1);
+        write(1, "\r", 1);
+        *fps_timer = 0;
+    }
+    (*fps_timer)++;
+}
+
+void    update_images(t_global *global)
+{
+    mlx_delete_image(global->mlx, global->minimap);
+    mlx_delete_image(global->mlx, global->img);
+    global->minimap = mlx_new_image(global->mlx, global->window_width
+                                                 * global->minimap_scale, global->window_height
+                                                                          * global->minimap_scale);
+    global->img = mlx_new_image(global->mlx, global->window_width,
+                                global->window_height);
+}
+
+void    draw_crosshair(t_global *global)
+{
+    t_line crosshair[2];
+
+    crosshair[0].a = (t_vec2d){global->window_width / 2 - 10,
+                               global->window_height / 2};
+    crosshair[0].b = (t_vec2d){global->window_width / 2 + 10,
+                               global->window_height / 2};
+    crosshair[1].a = (t_vec2d){global->window_width / 2, global->window_height
+                                                         / 2 - 10};
+    crosshair[1].b = (t_vec2d){global->window_width / 2, global->window_height
+                                                         / 2 + 10};
+    draw_line_crosshair(global, crosshair[0].a, crosshair[0].b, 0x00FF00);
+    draw_line_crosshair(global, crosshair[1].a, crosshair[1].b, 0x00FF00);
+}
+
+
+void    loop(void *param)
+{
+    t_global    *global;
+    static int  fps_timer = 0;
+    double      current_time;
+    double      elapsed_time;
     const double max_fps = 60.0;
     const double frame_duration = 1000.0 / max_fps;
 
@@ -65,55 +119,25 @@ void	loop(void *param)
     elapsed_time = current_time - global->time;
     global->time = current_time;
 
-    // Ensure the frame time is at least 16.67 milliseconds
-    if (elapsed_time < frame_duration)
-    {
-        my_usleep((frame_duration - elapsed_time) * 1000); // usleep takes microseconds
-        current_time = get_current_millis();
-        elapsed_time = current_time - global->time;
-        global->time = current_time;
-    }
+    cap_fps(&elapsed_time, frame_duration, global);
+    display_fps(&fps_timer, elapsed_time);
 
-    if (fps_timer % 10 == 0)
-    {
-        fps = (int)(1000.0 / elapsed_time);
-        write(1, "FPS: ", 5);
-        ft_putnbr_fd(fps, 1);
-        write(1, "\r", 1);
-        fps_timer = 0;
-    }
-    fps_timer++;
     update_door_segments(global);
-	mlx_delete_image(global->mlx, global->minimap);
-	mlx_delete_image(global->mlx, global->img);
-	global->minimap = mlx_new_image(global->mlx, global->window_width
-			* global->minimap_scale, global->window_height
-			* global->minimap_scale);
-	global->img = mlx_new_image(global->mlx, global->window_width,
-			global->window_height);
-	make_background_transparent(global);
-	showMap(global);
-	show_sky_and_floor(global);
-	render_3d(global);
-	ft_texture_to_image(global, global->sprite_textures[global->sprite_index]);
-	crosshair[0].a = (t_vec2d){global->window_width / 2 - 10,
-		global->window_height / 2};
-	crosshair[0].b = (t_vec2d){global->window_width / 2 + 10,
-		global->window_height / 2};
-	crosshair[1].a = (t_vec2d){global->window_width / 2, global->window_height
-		/ 2 - 10};
-	crosshair[1].b = (t_vec2d){global->window_width / 2, global->window_height
-		/ 2 + 10};
-	draw_line_crosshair(global, crosshair[0].a, crosshair[0].b, 0x00FF00);
-	draw_line_crosshair(global, crosshair[1].a, crosshair[1].b, 0x00FF00);
-	mlx_image_to_window(global->mlx, global->minimap, 0, 0);
-	mlx_image_to_window(global->mlx, global->img, 0, 0);
-	if (SHOW_MINIMAP)
-	{
-		mlx_image_to_window(global->mlx, global->minimap, 100, 100);
-		if (global->minimap->count > 0)
-			mlx_set_instance_depth(global->minimap->instances, 1);
-	}
+    update_images(global);
+    make_background_transparent(global);
+    showMap(global);
+    show_sky_and_floor(global);
+    render_3d(global);
+    ft_texture_to_image(global, global->sprite_textures[global->sprite_index]);
+    draw_crosshair(global);
+    mlx_image_to_window(global->mlx, global->minimap, 0, 0);
+    mlx_image_to_window(global->mlx, global->img, 0, 0);
+    if (SHOW_MINIMAP)
+    {
+        mlx_image_to_window(global->mlx, global->minimap, 100, 100);
+        if (global->minimap->count > 0)
+            mlx_set_instance_depth(global->minimap->instances, 1);
+    }
 	mlx_image_to_window(global->mlx, global->img, 0, 0);
 	if (global->img->count > 0)
 		mlx_set_instance_depth(global->img->instances, 0);
