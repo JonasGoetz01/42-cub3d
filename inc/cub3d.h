0882f8308219435c4f3a5cc452c5836f35a0a0ac
Jonas Götz <jonas.goetz@egotec.com>
2024-07-22 15:49:31 +0200

    remove unused

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -26,27 +26,19 @@
 # define WIDTH 1080
 # define HEIGHT 720
 # define FOV 1.0471975512
-# define BPP 32
 # define MOVE_SPEED 0.7f
 # define MINIMAP_SCALE 0.3f
 # define SHOW_MINIMAP 1
 # define SHOW_RAYS 0
 # define SHOW_COLLISIONS 0
 # define SHOW_FOV 1
-# define WALL_BUFFER_DISTANCE 0.2f
 # define PLAYER_RADIUS 0.2f
 # define INTERACT_MAX 2.0f
 # define INTERACT_MIN 0.3f
 
 # define NC "\033[0m"
 # define RED "\033[31m"
-# define GREEN "\033[32m"
-# define YELLOW "\033[33m"
-# define BLUE "\033[34m"
-# define MAG "\033[35m"
-# define CYAN "\033[36m"
 # define BOLD "\033[1m"
-# define LINE "\033[4m"
 
 # define ERR_MALLOC "\033[31m \033[1m ERROR: Malloc failed\n\033[0m"
 # define ERR_OPEN "\033[31m \033[1m ERROR: Could not open file \033[0m"
@@ -61,7 +53,6 @@
 # define ERR_NO_ID "\033[31m \033[1m ERROR: Unrecognized identifier\n\033[0m"
 # define ERR_D_ID "\033[31m \033[1m ERROR: Double identifier\n\033[0m"
 # define ERR_COMMA "\033[31m \033[1m ERROR: Invalid position of commas\n\033[0m"
-# define ERR_FORMAT "\033[31m \033[1m ERROR: Invalid color format\n\033[0m"
 # define ERR_EMPTY "\033[31m \033[1m ERROR: Empty line in map\n\033[0m"
 # define ERR_ARG "\033[31m \033[1m ERROR: Invalid number of arguments\n\033[0m"
 # define ERR_DOOR "\033[31m \033[1m ERROR: Invalid door position\n\033[0m"
@@ -146,36 +137,6 @@ typedef struct s_add_segments
 	int					y;
 }						t_add_segments;
 
-typedef struct s_render
-{
-	int					bar_width;
-	int					bar_height;
-	int					i;
-	t_ray				*ray;
-	t_collision			*closest_collision;
-	float				distance;
-	int					center_y;
-	int					bottom_y;
-	int					top_y;
-	int					x;
-	float				hit_percentage;
-	int					color;
-	mlx_texture_t		texture;
-	uint8_t				*pixel;
-	int					draw_y;
-	int					texture_x;
-	int					texture_y;
-	float				perpendicular_distance;
-	float				z_buffer[WIDTH];
-	float				ray_angle;
-	float				player_angle;
-	float				angle_diff;
-	int					r;
-	int					g;
-	int					b;
-	int					j;
-}						t_render;
-
 typedef struct s_global
 {
 	double				time;
@@ -193,7 +154,6 @@ typedef struct s_global
 	int					window_width;
 	int					window_height;
 	int					i;
-	int					j;
 	bool				free_mouse;
 	t_texture			*texture;
 	t_color				floor;
@@ -203,7 +163,6 @@ typedef struct s_global
 	int					door_count;
 	bool				open;
 	bool				close;
-	t_render			ren;
 	t_line				**door_line;
 }						t_global;
 
@@ -238,37 +197,9 @@ void					make_background_transparent(t_global *global);
 void					render_3d(t_global *global);
 double					get_current_millis(void);
 void					key_hook(mlx_key_data_t keydata, void *param);
-
-void					loop(void *param);
 void					key_hook_movement(void *param);
-void					initMap(t_global *global);
-void					map_to_line_segments(t_global *global, t_line **lines,
-							int *line_count);
-void					draw_line(t_global *global, t_vec2d a, t_vec2d b,
-							int color);
-void					showMap(t_global *global);
-float					calculate_scale_factor(int map_width, int map_height,
-							int window_width, int window_height);
-void					scale_line_segments(t_line *lines, int line_count,
-							float scale_factor);
-int						get_rgba(int r, int g, int b, int a);
-void					draw_ray(t_global *global, t_ray *ray);
-t_player				*new_player(t_global *global, t_vec2d pos, t_vec2d dir);
-t_vec2d					ray_line_collision(t_ray *ray, t_line *line,
-							t_face *face);
-void					update_position(t_global *global, t_vec2d dir,
-							float speed);
-void					raycast(t_global *global);
-void					resize(int32_t width, int32_t height, void *param);
-t_vec2d					get_player_position(t_global *global);
-t_vec2d					get_player_direction(t_global *global);
-void					cursor(double xpos, double ypos, void *param);
 void					show_sky(t_global *global);
 void					show_floor(t_global *global);
-void					make_background_transparent(t_global *global);
-void					render_3d(t_global *global);
-double					get_current_millis(void);
-void					get_opponents(t_global *global);
 int						parse_and_validate(char *file, t_global *global);
 bool					valid_file(char *file, int flag);
 bool					valid_map(char **map, int height);
@@ -286,7 +217,6 @@ bool					parse_map(char *line, t_global *global);
 bool					parse_line(char *line, t_global *global);
 void					get_doors(t_global *global);
 float					get_distance(t_vec2d a, t_vec2d b);
-void					update_door_segments(t_global *global);
 void					put_pixel_if_in_bounds(t_global *global, int x, int y,
 							int color);
 void					plot_circle_points(t_global *global, t_circle *circle,
@@ -316,10 +246,8 @@ void					cap_fps(double *elapsed_time, double frame_duration,
 void					display_fps(int *fps_timer, double elapsed_time);
 void					update_images(t_global *global);
 void					init_to_null(t_global *global);
-void					check_inactive_lines(t_global *global);
 float					map_distance_to_height(float distance,
 							t_global *global);
-void					check_active_lines(t_global *global);
 void					detect_collisions(t_global *global, t_ray *tmp_ray);
 void					process_ray(t_global *global, int i, float player_angle,
 							float *z_buffer);
@@ -334,63 +262,8 @@ void					update_door_lines(t_global *global);
 void					find_closest_collision_door(t_ray *tmp_ray);
 bool					circle_line_collision(t_vec2d circle_center,
 							float radius, t_line line);
-void					ft_exit_free(t_global *global);
-void					loop(void *param);
-void					keyHook(void *param);
-void					map_to_line_segments(t_global *global, t_line **lines,
-							int *line_count);
-void					draw_line(t_global *global, t_vec2d a, t_vec2d b,
-							int color);
-void					draw_line_crosshair(t_global *global, t_vec2d a,
-							t_vec2d b, int color);
 void					show_map(t_global *global);
-float					calculate_scale_factor(int map_width, int map_height,
-							int window_width, int window_height);
-void					scale_line_segments(t_line *lines, int line_count,
-							float scale_factor);
-void					draw_circle(t_global *global, t_circle *circle,
-							int color);
-int						get_rgba(int r, int g, int b, int a);
-void					draw_ray(t_global *global, t_ray *ray);
-t_player				*new_player(t_global *global, t_vec2d pos, t_vec2d dir);
-t_vec2d					ray_line_collision(t_ray *ray, t_line *line,
-							t_face *face);
-void					update_position(t_global *global, t_vec2d dir,
-							float speed);
-void					rotate_player(t_global *global, float angle);
-void					raycast(t_global *global);
-void					resize(int32_t width, int32_t height, void *param);
-t_vec2d					get_player_position(t_global *global);
-t_vec2d					get_player_direction(t_global *global);
-void					cursor(double xpos, double ypos, void *param);
-void					show_sky_and_floor(t_global *global);
-void					make_background_transparent(t_global *global);
-void					render_3d(t_global *global);
-double					get_current_millis(void);
-void					key_hook(mlx_key_data_t keydata, void *param);
-int						parse_and_validate(char *file, t_global *global);
-bool					valid_file(char *file, int flag);
-bool					valid_map(char **map, int height);
-bool					double_identifier(char *identifier, t_global *global);
-int						ft_strcmp(const char *s1, const char *s2);
-int						strlen_tab_to_space(char *str);
-char					*strdup_tab_to_space(const char *s1, int len);
-bool					check_arg_amount(char **split, int amount);
-bool					valid_range(int *color);
-bool					check_identifier(char *line, t_global *global);
-bool					color_format(char **color);
-bool					strlen_check(char **split);
-int						map_size(char *file, t_global *global);
-bool					parse_map(char *line, t_global *global);
-bool					parse_line(char *line, t_global *global);
-void					get_doors(t_global *global);
-float					get_distance(t_vec2d a, t_vec2d b);
-double					point_line_distance(t_vec2d point, t_line *line);
 void					check_open_door(t_global *global);
-void					detect_collisions(t_global *global, t_ray *tmp_ray);
-void					find_closest_collision_door(t_ray *tmp_ray);
-void					map_to_line_segments(t_global *global, t_line **lines,
-							int *line_count);
 bool					is_wall_above(t_global *global, int x, int y);
 bool					is_wall_below(t_global *global, int x, int y);
 bool					is_wall_left(t_global *global, int x, int y);
