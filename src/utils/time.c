24102b36ab8b7364ea8374fbf9145a72ea6d4789
Jonas Götz <jonas.goetz01@web.de>
2024-07-22 17:34:00 +0200

    better free functions

--- src/utils/time.c
+++ src/utils/time.c
@@ -6,11 +6,20 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:26:41 by cgerling          #+#    #+#             */
-/*   Updated: 2024/07/22 11:42:13 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/07/22 17:27:52 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
+
+void	free_and_null(void **ptr)
+{
+	if (*ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
+}
 
 double	get_current_millis(void)
 {
