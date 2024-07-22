3933aeb0c5f6930675f894f72c3e03a93afd9669
Jonas Götz <jonas.goetz01@web.de>
2024-07-22 13:44:40 +0200

    split into multiple functions

--- src/hooks/key.c
+++ src/hooks/key.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:26:41 by cgerling          #+#    #+#             */
-/*   Updated: 2024/07/22 11:44:37 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/07/22 13:44:22 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,49 +31,65 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 		global->close = false;
 }
 
+void	handle_movement_keys(t_global *global, t_vec2d *dir)
+{
+	if (mlx_is_key_down(global->mlx, MLX_KEY_W))
+	{
+		dir->x += global->player->dir.x;
+		dir->y += global->player->dir.y;
+	}
+	if (mlx_is_key_down(global->mlx, MLX_KEY_S))
+	{
+		dir->x -= global->player->dir.x;
+		dir->y -= global->player->dir.y;
+	}
+	if (mlx_is_key_down(global->mlx, MLX_KEY_A))
+	{
+		dir->x += global->player->dir.y;
+		dir->y -= global->player->dir.x;
+	}
+	if (mlx_is_key_down(global->mlx, MLX_KEY_D))
+	{
+		dir->x -= global->player->dir.y;
+		dir->y += global->player->dir.x;
+	}
+}
+
+void	handle_rotation_keys(t_global *global)
+{
+	if (mlx_is_key_down(global->mlx, MLX_KEY_E))
+		rotate_player(global, 0.03f);
+	if (mlx_is_key_down(global->mlx, MLX_KEY_Q))
+		rotate_player(global, -0.03f);
+}
+
+void	normalize_direction(t_vec2d *dir)
+{
+	float	length;
+
+	length = sqrtf(dir->x * dir->x + dir->y * dir->y);
+	if (length != 0)
+	{
+		dir->x /= length;
+		dir->y /= length;
+	}
+}
+
 void	key_hook_movement(void *param)
 {
 	t_global	*global;
 	t_vec2d		dir;
 	float		move_speed;
-	float		length;
 
-	dir = (t_vec2d){0.0f, 0.0f};
 	global = (t_global *)param;
+	dir = (t_vec2d){0.0f, 0.0f};
 	move_speed = MOVE_SPEED * global->scale_factor / 8;
 	if (mlx_is_key_down(global->mlx, MLX_KEY_ESCAPE))
 		return (mlx_close_window(global->mlx));
 	if (mlx_is_key_down(global->mlx, MLX_KEY_LEFT_SHIFT))
 		move_speed *= 2;
-	if (mlx_is_key_down(global->mlx, MLX_KEY_W))
-	{
-		dir.x += global->player->dir.x;
-		dir.y += global->player->dir.y;
-	}
-	if (mlx_is_key_down(global->mlx, MLX_KEY_S))
-	{
-		dir.x -= global->player->dir.x;
-		dir.y -= global->player->dir.y;
-	}
-	if (mlx_is_key_down(global->mlx, MLX_KEY_A))
-	{
-		dir.x += global->player->dir.y;
-		dir.y -= global->player->dir.x;
-	}
-	if (mlx_is_key_down(global->mlx, MLX_KEY_D))
-	{
-		dir.x -= global->player->dir.y;
-		dir.y += global->player->dir.x;
-	}
-	if (mlx_is_key_down(global->mlx, MLX_KEY_E))
-		rotate_player(global, 0.03f);
-	if (mlx_is_key_down(global->mlx, MLX_KEY_Q))
-		rotate_player(global, -0.03f);
-	length = sqrtf(dir.x * dir.x + dir.y * dir.y);
-	if (length != 0)
-	{
-		dir.x /= length;
-		dir.y /= length;
-	}
+	handle_movement_keys(global, &dir);
+	handle_rotation_keys(global);
+	normalize_direction(&dir);
 	update_position(global, dir, move_speed);
 }
