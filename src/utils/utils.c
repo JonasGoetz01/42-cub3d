fb9e540860a2e2709b91d3f96850e99cfad49c7b
Jonas Götz <jonas.goetz01@web.de>
2024-07-22 17:40:26 +0200

    rename utils file

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,11 +6,19 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:26:41 by cgerling          #+#    #+#             */
-/*   Updated: 2024/07/22 17:27:52 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/07/22 17:38:01 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
+
+void	init_texture(t_global *global)
+{
+	global->texture->north = NULL;
+	global->texture->south = NULL;
+	global->texture->west = NULL;
+	global->texture->east = NULL;
+}
 
 void	free_and_null(void **ptr)
 {
