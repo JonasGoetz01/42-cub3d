94f0a1ecfb977aa9ed0e75b3eb1fbab180aefd5d
Jonas Götz <jonas.goetz01@web.de>
2024-07-22 15:19:32 +0200

    norm

--- src/map/add_segments.c
+++ src/map/add_segments.c
@@ -3,38 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   add_segments.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgerling <cgerling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 13:14:44 by cgerling          #+#    #+#             */
-/*   Updated: 2024/07/22 14:33:47 by cgerling         ###   ########.fr       */
+/*   Updated: 2024/07/22 15:19:13 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
 
 void	add_wall_segments(t_global *global, t_line **lines, int *line_count,
-			t_add_segments *seg)
+		t_add_segments *seg)
 {
-	if (!is_wall_above(global, seg->x, seg->y)
-		&& !is_door_above(global, seg->x, seg->y))
+	if (!is_wall_above(global, seg->x, seg->y) && !is_door_above(global, seg->x,
+			seg->y))
 	{
 		add_line_segment(lines, line_count, set_wall_segment(seg->top_left,
 				seg->top_right, HORIZONTAL));
 	}
-	if (!is_wall_below(global, seg->x, seg->y)
-		&& !is_door_below(global, seg->x, seg->y))
+	if (!is_wall_below(global, seg->x, seg->y) && !is_door_below(global, seg->x,
+			seg->y))
 	{
 		add_line_segment(lines, line_count, set_wall_segment(seg->bottom_left,
 				seg->bottom_right, HORIZONTAL));
 	}
-	if (!is_wall_left(global, seg->x, seg->y)
-		&& !is_door_left(global, seg->x, seg->y))
+	if (!is_wall_left(global, seg->x, seg->y) && !is_door_left(global, seg->x,
+			seg->y))
 	{
 		add_line_segment(lines, line_count, set_wall_segment(seg->top_left,
 				seg->bottom_left, VERTICAL));
 	}
-	if (!is_wall_right(global, seg->x, seg->y)
-		&& !is_door_right(global, seg->x, seg->y))
+	if (!is_wall_right(global, seg->x, seg->y) && !is_door_right(global, seg->x,
+			seg->y))
 	{
 		add_line_segment(lines, line_count, set_wall_segment(seg->top_right,
 				seg->bottom_right, VERTICAL));
@@ -42,20 +42,20 @@ void	add_wall_segments(t_global *global, t_line **lines, int *line_count,
 }
 
 void	vertical_door_segment(t_line **lines, int *line_count,
-			t_add_segments *seg)
+		t_add_segments *seg)
 {
 	seg->left_middle = (t_vec2d){seg->x + 0.5, seg->y};
 	seg->right_middle = (t_vec2d){seg->x + 0.5, seg->y + 1};
 	add_line_segment(lines, line_count, set_door_segment(seg->left_middle,
 			seg->right_middle, VERTICAL, seg->door));
 	add_line_segment(lines, line_count, set_door_side_segment(seg->top_left,
-			seg->top_right, VERTICAL, seg->door));
+			seg->top_right, HORIZONTAL, seg->door));
 	add_line_segment(lines, line_count, set_door_side_segment(seg->bottom_left,
-			seg->bottom_right, VERTICAL, seg->door));
+			seg->bottom_right, HORIZONTAL, seg->door));
 }
 
 void	horizontal_door_segment(t_line **lines, int *line_count,
-			t_add_segments *seg)
+		t_add_segments *seg)
 {
 	seg->left_middle = (t_vec2d){seg->x, seg->y + 0.5};
 	seg->right_middle = (t_vec2d){seg->x + 1, seg->y + 0.5};
@@ -68,7 +68,7 @@ void	horizontal_door_segment(t_line **lines, int *line_count,
 }
 
 void	add_door_segments(t_global *global, t_line **lines, int *line_count,
-			t_add_segments *seg)
+		t_add_segments *seg)
 {
 	seg->door = NULL;
 	seg->i = 0;
@@ -86,8 +86,8 @@ void	add_door_segments(t_global *global, t_line **lines, int *line_count,
 	}
 	if (!seg->door)
 		ft_exit_free(global);
-	if (is_wall_left(global, seg->x, seg->y)
-		&& is_wall_right(global, seg->x, seg->y))
+	if (is_wall_left(global, seg->x, seg->y) && is_wall_right(global, seg->x,
+			seg->y))
 	{
 		horizontal_door_segment(lines, line_count, seg);
 	}
